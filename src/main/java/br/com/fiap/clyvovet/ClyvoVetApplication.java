88ae61c1e7ae75cd7edefbd52fe9480c4c6d7e2c
Vinicius-R-Cardozo <virc06@gmail.com>
2026-09-13 16:33:55 -0300

    Cria o projeto Spring Boot com Maven Wrapper

---
+++ src/main/java/br/com/fiap/clyvovet/ClyvoVetApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.clyvovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvoVetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClyvoVetApplication.class, args);
+    }
+}
